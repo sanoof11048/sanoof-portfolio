35805c9ea73a5c8c2b88c9529e241d807fe5cf49
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-06-30 15:40:51 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,20 +2,18 @@ const CACHE_NAME = "portfolio-cache-v1";
 const urlsToCache = [
   "index.html",
   "style.css",
-  "icons/icon-192.png",
-  "icons/icon-512.png"
+  "assets/icons/icon-192.png",
+  "assets/icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
